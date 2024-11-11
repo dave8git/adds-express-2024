c657016b62249528ee733612a57cb4ebe39632ae
dave8git <dave8git@gmail.com>
2024-11-11 23:43:34 +0100

    Add posts showing three in one line on mainpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { loadPostsRequest, getPosts } from './redux/postsReducer';
+import MiniPost from './components/MiniPost/MiniPost';
+
 function App() {
   const dispatch = useDispatch();
   const posts = useSelector(getPosts);
@@ -10,22 +13,16 @@ function App() {
   }, [dispatch]);
 
   return (
-    <div>
+    <div className="container">
       <h1>Posts</h1>
       {posts && posts.length > 0 ? (
-        posts.map((post) => (
-          <div key={post.id}>
-            <h2>{post.title}</h2>
-            <p>{post.content}</p>
-            {post.image && (
-              <img
-              src={`http://localhost:8000/public/${post.image}`}
-                alt={post.title}
-                style={{ width: '100%', maxWidth: '500px' }}
-              />
-            )}
-          </div>
-        ))
+        <div className="row">
+          {posts.map((post) => (
+            <div className="col-md-4" key={post.id}>
+              <MiniPost post={post} />
+            </div>
+          ))}
+        </div>
       ) : (
         <p>No posts available.</p>
       )}
---
+++ client/src/components/MiniPost/MiniPost.js
@@ -0,0 +1,27 @@
+// Component for displaying mini posts on the website 
+// Here you can find read more button which will take You to the full Post/Ad with edit buttons
+
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+
+function MiniPost({ post }) {
+  return (
+    <Card key={post.id} className="mb-4" style={{ maxWidth: '500px', margin: 'auto' }}>
+      {post.image && (
+        <Card.Img
+          variant="top"
+          src={`http://localhost:8000/public/${post.image}`}
+          alt={post.title}
+          style={{ width: '100%', height: 'auto' }}
+        />
+      )}
+      <Card.Body>
+        <Card.Title>{post.title}</Card.Title>
+        <Card.Text>{post.content}</Card.Text>
+        <Button variant="primary">Read More</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default MiniPost;
