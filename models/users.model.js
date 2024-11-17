e7adbf07165750aa411222257ab202d37da85ff2
dave8git <dave8git@gmail.com>
2024-11-17 11:47:54 +0100

    Add changes to users model

--- models/users.model.js
+++ models/users.model.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   login: { type: String, required: true, minlength: 1, maxlength: 10 },
   password: { type: String, required: true, minlength: 8 },
   avatar: { type: String, required: true },
-  //photo: { type: String, required: false },
+  phone: { type: String, required: false },
     
 });
 
